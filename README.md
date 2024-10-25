8a31b3dd3369a67efe6be0b04e49716f0c47b139
Peter Buckingham <peterbuckingham177@gmail.com>
2024-10-25 19:43:19 +0200

    styles done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Odin-foundations-calculator
 The Last project in the Odin Foundations course
+
+link to the project instructions (https://www.theodinproject.com/lessons/foundations-calculator#assignment)
+
---
+++ script.js
@@ -0,0 +1,13 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let divide = (a, b) => a / b;
+let multiply = (a, b) => a * b;
+
+//Operators Displayed
+let firstNum = 0;
+let secondNum = 0;
+let operator = '';
+
+function operate(numOne, operators, numTwo) {
+    return add(numOne, numTwo);
+}
